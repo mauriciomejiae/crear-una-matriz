4e6ff505968f796b8617a01c5bb7853225461e70
Mauricio Mejia Estevez <mme0504@gmail.com>
2018-08-06 00:34:23 -0500

    v1
    v1

---
+++ crear una matriz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crear_una_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombre = new string[100]; //100 cantidad de cajones
+            nombre[0] = "Juan";
+            nombre[1] = "Pedro";
+            nombre[2] = "Diego";
+            nombre[3] = "Mauricio";
+
+            Console.WriteLine(nombre[0]);
+            Console.WriteLine(nombre[1]);
+            Console.WriteLine(nombre[2]);
+            Console.WriteLine(nombre[3]);
+            Console.ReadKey();
+
+        }
+    }
+}
